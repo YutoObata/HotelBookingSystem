59e09eed95ffaa2b5cb1de0bdda14575b0803045
YutoObata <yobata@bulletgroup.jp>
2023-06-06 16:00:47 +0900

    簡易的なログイン画面だけ作成

--- demo/src/main/java/com/example/demo/MenuController.java
+++ demo/src/main/java/com/example/demo/MenuController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MenuController {
@@ -10,5 +12,38 @@ public class MenuController {
     @GetMapping("/")
     public String showMenu() {
         return "menu";
+    }
+
+    @PostMapping("/select")
+	public String selectMode(@RequestParam("mode") String mode) {
+		if (mode.equals("gotoBooking")) {
+			// 予約画面へ
+			return "login";
+		} else if (mode.equals("gotoManage")) {
+			// 管理者画面へ
+			return "manager";
+		} else {
+			return "menu";
+		}
+	}
+
+    // @GetMapping("/login")
+    // public String showLoginPage() {
+    //     return "login";
+    // }
+
+    @PostMapping("/dashboard")
+    public String login(String username, String password) {
+        // ログイン処理
+        if (username.equals("user") && password.equals("1234")) {
+            return "conpleted";
+        } else {
+            return "login";
+        }
+    }
+
+    @GetMapping("/dashboard")
+    public String showDashboard() {
+        return "conpleted";
     }
 }
