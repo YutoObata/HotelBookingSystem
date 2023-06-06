9529fa441a17e52b0f1c8415547593cacad09fd9
YutoObata <yobata@bulletgroup.jp>
2023-06-06 13:16:17 +0900

    メニュー画面にボタンを追加
    まだ画面遷移はできない

--- demo/src/main/java/com/example/demo/MenuController.java
+++ demo/src/main/java/com/example/demo/MenuController.java
@@ -1,5 +1,14 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MenuController {
     
+    // ローカルホストではmenu.htmlを開く
+    @GetMapping("/")
+    public String showMenu() {
+        return "menu";
+    }
 }
