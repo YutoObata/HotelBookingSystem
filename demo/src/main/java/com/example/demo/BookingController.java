5adce8e7c5bb17f2d0191210921d9ef748314c5b
YutoObata <yobata@bulletgroup.jp>
2023-06-08 17:31:40 +0900

    ボタンを押したら数をカウントできる機能
    今は使わない
    大人子供の人数の時に使う予定

--- demo/src/main/java/com/example/demo/BookingController.java
+++ demo/src/main/java/com/example/demo/BookingController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -60,6 +61,11 @@ public class BookingController {
 		userData.setChild(children);
 		model.addAttribute("userData", userData);
 		return "selectRoom";
+    }
+
+    @GetMapping("/increase")
+    public String countNumber() {
+        return "selectDate";
     }
 
 
