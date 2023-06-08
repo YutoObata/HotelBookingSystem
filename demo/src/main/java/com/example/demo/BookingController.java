302c513ad5b9f8fb5f2e4f7626260475f4f0be97
YutoObata <yobata@bulletgroup.jp>
2023-06-08 11:42:18 +0900

    客室タイプも結果画面で表示
    valueから参照することで実現

--- demo/src/main/java/com/example/demo/BookingController.java
+++ demo/src/main/java/com/example/demo/BookingController.java
@@ -2,14 +2,13 @@ package com.example.demo;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,9 +30,6 @@ public class BookingController {
     /* -------------------
         日程の選択
     -------------------- */
-    private Map<String, Integer> numberOfAdult;
-    private Map<String, Integer> numberOfChild;
-
     @PostMapping("/select/dateSelect")
     public String selectDate(@RequestParam("checkInDate") String checkInDate,
             @RequestParam("checkOutDate") String checkOutDate, @RequestParam("adult") int adults,
@@ -83,18 +79,12 @@ public class BookingController {
         部屋の選択
     -------------------- */
     @PostMapping("/select/roomSelect")
-    public String selectRoom(@RequestParam("roomSelect") String room, Model model) {
-        if (room.equals("economy")) {
-            model.addAttribute("room", "economy");
-            return "booking";
-        } else if (room.equals("standard")) {
-            model.addAttribute("room", "standard");
-            return "booking";
-        } else {
-            return "selectRoom";
-        }
-    }
-
+	public String selectRoom(@RequestParam("roomSelect") String room, Model model) {
+		UserData userData = new UserData();
+		userData.setRoom(room);
+		model.addAttribute("userData", userData);
+		return "booking";
+	}
 
     /* -------------------
         個人情報入力
@@ -106,14 +96,16 @@ public class BookingController {
         }
 
         UserData existingData = userRepository.findByName(userData.getName());
-        if (existingData != null) {
-            existingData.setTel(userData.getTel());
-            userRepository.save(existingData);
-        } else {
-            userRepository.save(userData);
-        }
+		if (existingData != null) {
+			existingData.setTel(userData.getTel());
+			existingData.setRoom(userData.getRoom()); // Set the selected room value
+			userRepository.save(existingData);
+		} else {
+			userData.setRoom(userData.getRoom()); // Set the selected room value
+			userRepository.save(userData);
+		}
 
-        model.addAttribute("userDataList", userRepository.findAll());
-        return "completed";
-    }
+		model.addAttribute("userDataList", userRepository.findAll());
+		return "completed";
+	}
 }
