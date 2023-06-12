af87d08578f9d127848f6e735f1ae275b5bcd55a
YutoObata <yobata@bulletgroup.jp>
2023-06-12 16:48:19 +0900

    個人情報入力画面の支払い方法

--- demo/src/main/resources/static/JS/booking.js
+++ demo/src/main/resources/static/JS/booking.js
@@ -14,3 +14,35 @@ $(document).ready(function(){
         $(this).parent().children('.form-style').focus();
 	});
 });
+
+
+function handlePaymentOption() {
+	var localPaymentRadio = document.getElementById("howToPay");
+	var cardNumberInput = document.querySelector(".form-style-number");
+	var cardExpirationMonthSelect = document.querySelectorAll(".form-item-card")[0];
+	var cardExpirationYearSelect = document.querySelectorAll(".form-item-card")[1];
+	var securityCodeInput = document.querySelector(".security-code");
+  
+	if (localPaymentRadio.checked) {
+	  // 現地払いが選択された場合、入力フィールドをグレーにして無効化する
+	  cardNumberInput.style.backgroundColor = "#ccc";
+	  cardNumberInput.disabled = true;
+	  cardExpirationMonthSelect.style.backgroundColor = "#ccc";
+	  cardExpirationMonthSelect.disabled = true;
+	  cardExpirationYearSelect.style.backgroundColor = "#ccc";
+	  cardExpirationYearSelect.disabled = true;
+	  securityCodeInput.style.backgroundColor = "#ccc";
+	  securityCodeInput.disabled = true;
+	} else {
+	  // 現地払い以外が選択された場合、入力フィールドのスタイルと有効性をリセットする
+	  cardNumberInput.style.backgroundColor = "";
+	  cardNumberInput.disabled = false;
+	  cardExpirationMonthSelect.style.backgroundColor = "";
+	  cardExpirationMonthSelect.disabled = false;
+	  cardExpirationYearSelect.style.backgroundColor = "";
+	  cardExpirationYearSelect.disabled = false;
+	  securityCodeInput.style.backgroundColor = "";
+	  securityCodeInput.disabled = false;
+	}
+  }
+  
